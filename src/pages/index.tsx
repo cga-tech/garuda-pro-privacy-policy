c3801b27bcb106f95070ed633a19f44f6d3e887a
renakaagusta <renakaagusta28@gmail.com>
2023-02-07 09:00:07 +0700

    Fix basic account information description

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,9 +32,7 @@ export default function HomePage() {
               account through our admin portal, they must provide us with some
               personal information, such as your name, username, password, email
               address, or phone number. On Garuda, your name and username are
-              always listed publicly, including on your profile page and in
-              search results, and you can use either your real name or a
-              pseudonym. You can create and manage multiple Twitter accounts.
+              always listed publicly.
               <br />
               <br />
               <b>Contact Information</b> : Your school or you may use your
